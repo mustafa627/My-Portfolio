6e1ce8a7975ca0020c0eded94211157850d8f118
Ghulam Mustafa <mustafaattarihafizghulam@gmail.com>
2025-07-06 19:26:23 +0500

    photo route change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['CtrlF'], // 👈 isko replace karo agar module ka actual naam alag ho
+    },
+  },
 })
