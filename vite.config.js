f1a87f76310d130d0bf59644c33a8dec37c466c6
Ghulam Mustafa <mustafaattarihafizghulam@gmail.com>
2025-07-06 19:59:14 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['CtrlF'], // 👈 isko replace karo agar module ka actual naam alag ho
-    },
-  },
 })
